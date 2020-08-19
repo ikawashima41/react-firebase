7502c0b6ab9d66f07577cc634db5170d055a4498
ikawashima-cq <i.kawashima@caraquri.com>
2020-08-19 10:17:43 +0900

    Add some functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,28 @@
 import * as functions from "firebase-functions";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 import admin from "firebase-admin";
-import { collection } from "./services/mangarel/constants";
+import puppeteer from "puppeteer";
+
+import { collectionName } from "./services/mangarel/constants";
+import { feedCalendar } from "./crawlers/kodansha-calender";
+import { saveFeedMemos } from "./firestore-admin/feed-memo";
+
+const PUPPETEER_OPTIONS = {
+  args: [
+    "--disable-gpu",
+    "--disable-dev-shm-usage",
+    "--disable-setuid-sandbox",
+    "--no-first-run",
+    "--no-sandbox",
+    "--no-zygote",
+    "--single-process",
+  ],
+  headless: true,
+};
 
 admin.initializeApp();
 
 export const publishers = functions
-  .region("asia-northease1")
+  .region("asia-northeast1")
   .https.onRequest(async (req, res) => {
     const snap = await admin
       .firestore()
@@ -21,4 +30,23 @@ export const publishers = functions
       .get();
     const data = snap.docs.map((doc) => doc.data());
     res.send({ data });
+  });
+
+export const fetchCarender = functions
+  .region("asia-northeast1")
+  .runWith({
+    timeoutSeconds: 300,
+    memory: "2GB",
+  })
+  .pubsub.schedule("0 2 1,10,20 * *")
+  .timeZone("Asis/Tokyo")
+  .onRun(async () => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+    const page = await browser.newPage();
+    const db = admin.firestore();
+
+    const memos = await feedCalendar(page);
+    const fetchCount = await saveFeedMemos(db, memos, "kodansya");
+    await browser.close();
+    console.log(`Fetched Kodansha calendar. Wrote ${fetchCount} memos.`);
   });
